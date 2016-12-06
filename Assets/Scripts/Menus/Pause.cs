5c06f92b1ba9b5034728bbe15afcedc619b07aa2
CrazyLegumes <deepwns@live.com>
2016-12-05 22:33:30 -0600

    Resume Button
    Resume Button Plays Tweens

--- Assets/Scripts/Menus/Pause.cs
+++ Assets/Scripts/Menus/Pause.cs
@@ -63,6 +63,7 @@ public class Pause : MonoBehaviour {
 
 
         isPaused = false;
+        DOTween.PlayAll();
         GameManager.Instance.state = game.game;
         UpdateDisplay();
         UpdateManagerState();
