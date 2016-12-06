3fbb54f9c80340db548888fb69ead10cb75db19f
CrazyLegumes <deepwns@live.com>
2016-12-05 23:12:45 -0600

    Teleporter
    AddedScene Switching after level is completed

---
+++ Assets/Scripts/Objects/Teleporter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+    private bool IsTriggered;
+    [SerializeField]
+    private string player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        IsTriggered = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == player)
+        {
+            IsTriggered = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == player)
+        {
+            IsTriggered = false;
+        }
+    }
+
+
+    public bool Triggered()
+    {
+        return IsTriggered;
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Objects/TeleporterTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TeleporterTrigger : MonoBehaviour {
+
+    [SerializeField]
+    private string NextScene;
+
+    Teleporter[] TeleporterList;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        TeleporterList= this.gameObject.GetComponentsInChildren<Teleporter>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(TeleporterList[0].Triggered() && TeleporterList[1].Triggered())
+        {
+            SceneManager.LoadScene(NextScene);
+        }
+	}
+
+
+
+
+}
